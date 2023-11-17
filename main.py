d431048e68d03624254b01621c657d28a4d03f03
Olav Bjørlykke <olav.bjorlykke@gmail.com>
2023-11-17 17:24:25 +0100

    Renamed the gurobi model for a single site to sub_problem.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ input =InputData()
 
 scenarios_test = Scenarios(input.temperatures_df)
 
+print(input.TGC_df)
+
 
 site_test = Site(
     scenario_temperatures=scenarios_test.scenario_temperatures_per_site_df.loc["Senja"],
@@ -17,15 +19,14 @@ site_test = Site(
     init_biomass=100,
     TGC_array=input.TGC_df.iloc[1],
     smolt_weights=[150, 200, 250],
-    weight_req_for_harvest = 3000
+    weight_req_for_harvest = 3000.0
 )
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 
-print("GROWT SETS:", site_test.growth_sets)
+#print("GROWTH SETS:", site_test.growth_sets)
 
-site_test.weight_dev_per_scenario_df.index.names = ["weight", "scenario", "period"]
 
 
 
