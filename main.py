f702776583226faf0c542a059f44498bf548979e
Olav Bjørlykke <olav.bjorlykke@gmail.com>
2024-01-15 14:18:04 +0100

    Added in name = main idiom to main file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import subproblem_list
 from orchestration_class import Orchestration
 
+def main():
+    orchestration = Orchestration(
+        subproblems=subproblem_list.SUB_PROBLEM_LIST
+    )
 
-orchestration = Orchestration(
-    subproblems=subproblem_list.SUB_PROBLEM_LIST
-)
+    orchestration.run_branching_algorithm()
 
-orchestration.run_branching_algorithm()
+    orchestration.print_explored_node_to_file(orchestration.explored_nodes)
 
-orchestration.print_explored_node_to_file(orchestration.explored_nodes)
+
+if __name__ == "__main__":
+    main()
 
 
 
